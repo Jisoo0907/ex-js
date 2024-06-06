39bea6cbd23fd5c7e231b711d86a3fe91d87996d
jisoolee <szjslee@gmail.com>
2024-06-07 01:02:52 +0900

    reduce로 평균 구하기

--- src/js/05.function.js
+++ src/js/05.function.js
@@ -23,3 +23,11 @@ const sum = [1, 2, 3, 4, 5].reduce((accumulate, currentValue) => {
 });
 
 console.log(sum);
+
+/* 평균 구하기 */
+const values = [1, 2, 3, 4, 5, 6];
+const average = values.reduce((acc, cur, i, { length }) => {
+  return i === length - 1 ? (acc + cur) / length : acc + cur;
+}, 0);
+
+console.log(average);
