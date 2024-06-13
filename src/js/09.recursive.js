fe88e45fb7d8920a44c04fb2816dfa9c4ae09db7
jisoolee <szjslee@gmail.com>
2024-06-14 01:03:07 +0900

    pow 함수 이해 완료

--- src/js/09.recursive.js
+++ src/js/09.recursive.js
@@ -39,3 +39,22 @@ sumSalaries(company); // 7700
   }
 }
 pow(2, 3); */
+
+/* function pow(x, n) {
+  let result = 1;
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+
+*/
+function pow(x, n) {
+  if (n === 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+
+console.log(pow(2, 3)); // 8
